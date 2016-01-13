20d8186b6361a2e276a5f09b95e0e694318d3fc7
Askarbek Karasasev <askar.karasaev@gmail.com>
2016-01-13 17:15:46 +0900

    setting stable channel

--- config.rb
+++ config.rb
@@ -54,6 +54,7 @@ end
 
 # Official CoreOS channel from which updates should be downloaded
 #$update_channel='alpha'
+$update_channel='stable'
 
 # Log the serial consoles of CoreOS VMs to log/
 # Enable by setting value to true, disable with false
